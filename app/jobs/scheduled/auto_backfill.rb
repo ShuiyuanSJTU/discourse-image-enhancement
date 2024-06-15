9ceb07beea8da1b57537f2b37fb87422fce4c81c
pangbo13 <51732678+pangbo13@users.noreply.github.com>
2024-06-15 19:36:57 +0800

    fix: SiteSetting

--- app/jobs/scheduled/auto_backfill.rb
+++ app/jobs/scheduled/auto_backfill.rb
@@ -6,7 +6,7 @@ module Jobs
       every 1.hour
 
       def execute(_args)
-        return unless SiteSetting.discourse_image_enhancement_enabled && SiteSetting.image_search_enabled
+        return unless SiteSetting.image_enhancement_enabled && SiteSetting.image_search_enabled
         start_time = Time.now
         backfill_posts = ::DiscourseImageEnhancement::ImageSearch.Filter
           .posts_need_analysis
--- app/jobs/scheduled/auto_cleanup.rb
+++ app/jobs/scheduled/auto_cleanup.rb
@@ -6,7 +6,7 @@ module Jobs
       every 3.days
 
       def execute(_args)
-        return unless SiteSetting.discourse_image_enhancement_enabled && SiteSetting.image_search_enabled
+        return unless SiteSetting.image_enhancement_enabled && SiteSetting.image_search_enabled
         ::DiscourseImageEnhancement::Filter.image_search_data_need_remove.destroy_all
       end
     end
