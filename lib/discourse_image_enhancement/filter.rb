7f9487a3341cec59914c2fa4a8716b900792759e
pangbo13 <51732678+pangbo13@users.noreply.github.com>
2024-06-15 17:01:43 +0800

    dev: filter

--- lib/discourse_image_enhancement/filter.rb
+++ lib/discourse_image_enhancement/filter.rb
@@ -38,9 +38,15 @@ module ::DiscourseImageEnhancement
       uploads
     end
 
-    def self.posts_need_analysis(exclude_existing: true)
-      filter_post(Post).joins(:uploads)
+    def self.posts_need_analysis(exclude_existing: true, max_images_per_post: SiteSetting.image_enhancement_max_images_per_post)
+      posts = filter_post(Post).joins(:uploads)
         .merge(filter_upload(Upload, exclude_existing: exclude_existing))
+      if max_images_per_post.present? && max_images_per_post > 0
+        posts = posts.group('posts.id')
+        .having('COUNT(uploads.id) <= ? AND COUNT(uploads.id) > 0',SiteSetting.image_enhancement_max_images_per_post)
+        .select('posts.*')
+      end
+      posts.distinct
     end
 
     def self.uploads_need_analysis(exclude_existing: true)
@@ -50,7 +56,9 @@ module ::DiscourseImageEnhancement
 
     def self.image_search_data_need_remove
       ImageSearchData.where.not(sha1:
-        uploads_need_analysis(exclude_existing: false).joins("JOIN image_search_data ON image_search_data.sha1 = COALESCE(uploads.original_sha1, uploads.sha1)").select("image_search_data.sha1")
+        uploads_need_analysis(exclude_existing: false)
+          .joins("JOIN image_search_data ON image_search_data.sha1 = COALESCE(uploads.original_sha1, uploads.sha1)")
+          .select("image_search_data.sha1")
       )
     end
   end
