5844940ccf3c82b3f7ec814f345c65252c708ed7
pangbo13 <51732678+pangbo13@users.noreply.github.com>
2024-06-21 03:01:11 +0800

    fix: backfill job

--- app/jobs/scheduled/auto_backfill.rb
+++ app/jobs/scheduled/auto_backfill.rb
@@ -8,7 +8,7 @@ module Jobs
       def execute(_args)
         return unless SiteSetting.image_enhancement_enabled && SiteSetting.image_search_enabled
         start_time = Time.now
-        backfill_posts = ::DiscourseImageEnhancement::ImageSearch::Filter
+        backfill_posts = ::DiscourseImageEnhancement::Filter
           .posts_need_analysis
           .distinct
         backfill_posts.find_each do |post|
