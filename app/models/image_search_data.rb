799f3cfdd08d6b419db8b82d066b788654501c33
pangbo13 <51732678+pangbo13@users.noreply.github.com>
2024-07-12 19:09:09 +0000

    chore: add model annotation

--- app/models/image_search_data.rb
+++ app/models/image_search_data.rb
@@ -7,3 +7,20 @@ class ImageSearchData < ActiveRecord::Base
     self.where(sha1: post.uploads.pluck("COALESCE(uploads.original_sha1, uploads.sha1)").uniq)
   end
 end
+
+# == Schema Information
+#
+# Table name: image_search_data
+#
+#  sha1                    :string           not null, primary key
+#  ocr_text                :text
+#  description             :text
+#  ocr_text_search_data    :tsvector
+#  description_search_data :tsvector
+#
+# Indexes
+#
+#  index_image_search_data_on_description_search_data  (description_search_data) USING gin
+#  index_image_search_data_on_ocr_text_search_data     (ocr_text_search_data) USING gin
+#  index_image_search_data_on_sha1                     (sha1) UNIQUE
+#
