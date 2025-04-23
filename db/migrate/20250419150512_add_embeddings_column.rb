11db43aa0bd1b7229bea5a9bc0ac920e31337301
pangbo13 <51732678+pangbo13@users.noreply.github.com>
2025-04-23 18:29:16 +0800

    fix: enable extension during migration

--- db/migrate/20250419150512_add_embeddings_column.rb
+++ db/migrate/20250419150512_add_embeddings_column.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 class AddEmbeddingsColumn < ActiveRecord::Migration[7.2]
   def change
+    enable_extension :vector
     add_column :image_search_data, :embeddings, :halfvec
     execute <<~SQL
       CREATE INDEX IF NOT EXISTS image_search_data_embeddings_index
